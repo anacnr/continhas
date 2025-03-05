743f8a248fa35be34b399bef32504afa12ca3651
anacnr <anacnrezende@gmail.com>
2025-03-05 16:04:26 -0300

    Operaçõs básicas.

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,3 +1,23 @@
 "use strict";
-let progr = "ana carolina";
-console.log(progr);
+document.addEventListener("DOMContentLoaded", () => {
+    function toSum(fisrt_parcel, second_parcel) {
+        let sum = fisrt_parcel + second_parcel;
+        console.log("Resultado: ", sum);
+    }
+    toSum(10, 2); //Função de soma
+    function toSubtract(minuend, subtrahend) {
+        let change_over = minuend - subtrahend;
+        console.log("Resultado: ", change_over);
+    }
+    toSubtract(10, 2); //Função de subtração
+    function toMultiply(fisrt_factor, second_factor) {
+        let product = fisrt_factor * second_factor;
+        console.log("Resultado: ", product);
+    }
+    toMultiply(10, 2); //Função de multiplicação
+    function toDivide(dividend, divider) {
+        let quotient = dividend / divider;
+        console.log("Resultado: ", quotient);
+    }
+    toDivide(10, 2); //Função de Divisão
+});
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,25 @@
-let progr : string = "ana carolina"
-console.log(progr);
+document.addEventListener("DOMContentLoaded" , ()=>{
+    function toSum(fisrt_parcel : number, second_parcel: number){
+        let sum : number = fisrt_parcel + second_parcel 
+        console.log("Resultado: " , sum);
+    }
+    toSum(10,2)//Função de soma
+
+    function toSubtract(minuend : number, subtrahend: number){
+        let change_over : number = minuend - subtrahend
+        console.log("Resultado: ", change_over);
+    }
+    toSubtract(10,2)//Função de subtração
+
+    function toMultiply(fisrt_factor : number, second_factor: number){
+        let product : number = fisrt_factor * second_factor
+        console.log("Resultado: ", product);
+    }
+    toMultiply(10,2)//Função de multiplicação
+
+    function toDivide(dividend : number, divider: number){
+        let quotient : number = dividend / divider
+        console.log("Resultado: ", quotient);
+    }
+    toDivide(10,2)//Função de Divisão
+});
